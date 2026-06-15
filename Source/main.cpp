a4fd971c0cd6df1cab779b7e925deaadc2eb0ec7
Ivan <elderivanargueta04@gmail.com>
2026-06-15 11:46:03 -0700

    Making bouncy particle again to remember wtf i was trying to do

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,89 +2,196 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <cmath>
+
 #include "../Header/Particle.h"
 
 float COR = .65f;
 float yVelThreshold = 0.2f;
-// float ECC =
+constexpr float PI = 3.14159265358979323846f;
+
+const char *vertexShader = R"GLSL(
+        #version 330 core
+        layout(location = 0) in vec3 aPos; // Current vertex we are handling from the vector of vertices
+
+        uniform vec2 uResolution; //holds screen W/H
+        uniform vec2 uOffset; // current position of the particle
+        uniform float uScale; //  particles radius
+
+        void main() {
+            vec2 worldPos = aPos.xy * uScale + uOffset; // only use the xy values of the vertices, scale it and add the current position to get real new position
+
+            vec2 ndc = worldPos / uResolution; // convert from pixel coordinated to normalized range [0,1]
+            ndc = ndc * 2.0 - 1.0; // convert from normalized range to normalized device coordinates [-1,1]
+
+            gl_Position = vec4(ndc.x, ndc.y, 0.0, 1.0); // Final position in clip space (OpenGL expects vec4 in NDC range)
+        }
+    )GLSL";
+
+const char *fragmentShader = R"GLSL(
+        #version 330 core
+        out vec4 FragColor; // color that will be sent out to screen
+        uniform vec3 uColor; // RGB
+        void main() {
+            FragColor = vec4(uColor, 1.0); // pass in the RBG plus alpha
+        }
+    )GLSL";
 
 int main() {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // glfwInit();
-    //
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //
-    // if (!glfwInit()) {
-    //     std:: cerr << "GLFW INIT ERROR \n";
-    //     return -1;
-    // }
-    //
-    // GLFWwindow *window = glfwCreateWindow(800, 800, "Hello World", NULL, NULL);
-    // if (window == nullptr) {
-    //     std:: cerr << "WINDOW ERROR, COULDN'T CREATE, WINDOW IS NULL\n";
-    //     glfwTerminate();
-    //     return -1;
-    // }
-    //
-    // glfwMakeContextCurrent(window);
-    // if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    //     std::cerr << "GLAD INIT ERROR\n";
-    //     return -1;
-    // }
-    //
-    // while (!glfwWindowShouldClose(window)) {
-    //     glClear(GL_COLOR_BUFFER_BIT);
-    //     glfwPollEvents();
-    //     glfwSwapBuffers(window);
-    // }
-    //
-    // glfwDestroyWindow(window);
-    // glfwTerminate();
+    if (!glfwInit()) {
+        std::cerr << "GLFW INIT ERROR \n";
+        return -1;
+    }
 
-    bool hitFloor = false;
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+    if (window == nullptr) {
+        std::cerr << "WINDOW ERROR, COULDN'T CREATE, WINDOW IS NULL\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cerr << "GLAD INIT ERROR\n";
+        return -1;
+    }
+
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShader, nullptr);
+    glCompileShader(vs);
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShader, nullptr);
+    glCompileShader(fs);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    GLuint uResolutionLoc = glGetUniformLocation(shaderProgram, "uResolution");
+    GLuint uColorLoc = glGetUniformLocation(shaderProgram, "uColor");
+    GLuint uOffsetLoc = glGetUniformLocation(shaderProgram, "uOffset");
+    GLuint uScaleLoc = glGetUniformLocation(shaderProgram, "uScale");
+
     bool isStill = false;
     Particle particle;
     particle.printPosition();
     particle.printVelocity();
     particle.printAcceleration();
 
-    particle.setPosition(0,10,0);
-    particle.setVelocity(1,0,0);
-    particle.setAcceleration(0,-9.8,0);
+    particle.setPosition(0, 10, 0);
+    particle.setVelocity(1, 0, 0);
+    particle.setAcceleration(0, -9.8, 0);
     particle.setDamping(1.0f);
     particle.setMass(5);
     particle.setRadius(10);
     particle.setKinetic(5);
 
     int loopCounter = 0;
-    while (isStill == false) {
-        particle.update(0.016f);
-        if (particle.getPosition().y <= 0.0f) {
-            Vector3 vel = particle.getVelocity();
-            Vector3 pos = particle.getPosition();
 
-            pos.y = 0;
-            vel.y = -vel.y * COR;
+    std:: vector<Vector3> circleVertices;
+    Vector3 center;
+    int segments = 32;
+    circleVertices.reserve(segments + 2);
+    circleVertices.emplace_back(center);
 
-            particle.setVelocity(vel);
-            particle.setPosition(pos);
-        }
+    for (int i = 0; i < segments; i++) {
+        float progress = static_cast<float>(i) / static_cast<float>(segments);
+        float theta = progress * 2.0f * PI;
 
-        if (particle.getPosition().y == 0 && std::abs(particle.getVelocity().y) <= yVelThreshold) {
-            particle.setAcceleration({});
-            particle.setVelocity({});
-            isStill = true;
-        }
-        particle.printPosition();
-        particle.printVelocity();
-        particle.printAcceleration();
-        loopCounter++;
-        std:: cout << std:: endl;
+        Vector3 position;
+
+        position.x = center.x + std::cos(theta) * particle.getRadius();
+        position.y = center.y + std::sin(theta) * particle.getRadius();
+        position.z = 0;
+
+        circleVertices.emplace_back(position);
     }
 
-    std:: cout << "Loops done: " << loopCounter << std:: endl;
+    GLuint VAO = 0;
+    GLuint VBO = 0;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER,circleVertices.size() * sizeof(Vector3),circleVertices.data(),GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+    while (!glfwWindowShouldClose(window)) {
+        auto currentTime = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> deltaTime = currentTime - startTime;
+        startTime = currentTime;
+        const auto dt = deltaTime.count();
+
+        int w = 1280;
+        int h = 720;
+
+        glfwGetFramebufferSize(window, &w, &h);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glUniform2f(uResolutionLoc, static_cast<float>(w), static_cast<float>(h));
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+        if (isStill == false) {
+            particle.update(dt);
+
+            if (particle.getPosition().y <= 0.0f) {
+                Vector3 pos = particle.getPosition();
+                Vector3 vel = particle.getVelocity();
+
+                pos.y = 0;
+                vel.y = -vel.y * COR;
+
+                if (std::abs(vel.y) <= yVelThreshold) {
+                    particle.setAcceleration({});
+                    isStill = true;
+                }
+
+                particle.setPosition(pos);
+                particle.setVelocity(vel);
+            }
+            particle.printPosition();
+            particle.printVelocity();
+            particle.printAcceleration();
+            std:: cout << "Delta time: " << dt << std::endl;
+            loopCounter++;
+            std::cout << std::endl;
+        }
+
+        glUniform2f(uOffsetLoc, particle.getPosition().x, particle.getPosition().y);
+        glUniform1f(uScaleLoc, particle.getRadius());
+        glDrawArrays(GL_TRIANGLE_FAN, 0, static_cast<GLsizei>(circleVertices.size()));
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    std::cout << "Loops done: " << loopCounter << std::endl;
 
     return 0;
 }
