c38226c151e7b1f5d8895abf79c82155cec2da47
Ivan <elderivanargueta04@gmail.com>
2026-06-21 01:30:28 -0700

    Tail printing issue
    There seems to be an extra tail that prints off screen. Made the screen size bigger and it loos like a copy is made idk why will look into it tmrw

--- Header/Constants.h
+++ Header/Constants.h
@@ -8,8 +8,8 @@
 const float COR = .65f;
 const float yVelThreshold = 0.2f;
 constexpr float PI = 3.14159265358979323846f;
-constexpr float W = 1280.0f;
-constexpr float H = 720.0f;
+constexpr float W = 1920.0f;
+constexpr float H = 1080.0f;
 const float MU = 155000.0f;
 
 #endif //PARTICLESIM_CONSTANTS_H
