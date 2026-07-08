51ebd8ceb5b8c278065130a41365992efab9b2b2
Ivan <elderivanargueta04@gmail.com>
2026-07-08 10:56:12 -0700

    Camera and sphere classes

--- Header/Camera.h
+++ Header/Camera.h
@@ -5,4 +5,40 @@
 #ifndef PARTICLESIM_CAMERA_H
 #define PARTICLESIM_CAMERA_H
 
+#include <glm/glm/glm.hpp>
+#include "glm/glm/gtc/matrix_transform.hpp"
+
+class Camera {
+public:
+    float yaw;
+    float pitch;
+    float speed;
+    bool firstMove;
+private:
+    glm::vec3 position;
+    glm::vec3 worldUp;
+
+public:
+
+    Camera();
+
+    glm::vec3 getDirection() const;
+
+    glm::vec3 getRight() const;
+
+    glm::mat4 getViewMatrix() const;
+
+    void setPosition(const glm::vec3 &passedPosition);
+    void setPosition(float x, float y, float z);
+    glm::vec3 &getPosition();
+
+    void setYaw(float passedYaw);
+    float &getYaw();
+
+    void setPitch(float passedPitch);
+    float &getPitch();
+
+
+};
+
 #endif //PARTICLESIM_CAMERA_H
